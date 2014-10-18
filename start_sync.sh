9902567da8ee361a11df477125195d62057aa451
Bence Kiglics <bence.kiglics@prezi.com>
2014-10-18 16:29:58 +0200

    added start sync script

---
+++ start_sync.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+
+cabal clean
+cabal install --only-dependencies
+cabal configure
+cabal build
+dist/build/player/player 1900
+
